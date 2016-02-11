29fa5ba79a990fd0e1b1a4cc93a0f0c54f921a41
Andreas Blaesius <skate4life@gmx.de>
2016-02-11 23:57:47 +0100

    Update tab2.sh

--- tab2.sh
+++ tab2.sh
@@ -43,20 +43,7 @@ echo "----------------------------------------"
 echo "----------------------------------------"
 cd hardware/ti/omap4
 echo "----------------------------------------"
-echo "android-api: add MM"
-git fetch https://gerrit.omnirom.org/android_hardware_ti_omap4 refs/changes/58/16158/1 && git cherry-pick FETCH_HEAD
-echo "----------------------------------------"
-echo "camera: fix build for MM"
-git fetch https://gerrit.omnirom.org/android_hardware_ti_omap4 refs/changes/59/16159/1 && git cherry-pick FETCH_HEAD
-echo "----------------------------------------"
-echo "domx: pass ANDROID_API_CFLAGS"
-git fetch https://gerrit.omnirom.org/android_hardware_ti_omap4 refs/changes/60/16160/1 && git cherry-pick FETCH_HEAD
-echo "----------------------------------------"
-echo "domx: Fix GetParameter crashing with OMX_IndexParamConsumerUsageBits"
-git fetch https://gerrit.omnirom.org/android_hardware_ti_omap4 refs/changes/61/16161/1 && git cherry-pick FETCH_HEAD
-echo "----------------------------------------"
-echo "camera: fix build for recent 6.0 releases"
-git fetch https://gerrit.omnirom.org/android_hardware_ti_omap4 refs/changes/62/16162/1 && git cherry-pick FETCH_HEAD
+git fetch https://gerrit.omnirom.org/android_hardware_ti_omap4 refs/changes/56/16856/1 && git cherry-pick FETCH_HEAD
 echo "----------------------------------------"
 cd ../../..
 echo "----------------------------------------"
@@ -77,14 +64,14 @@ echo "----------------------------------------"
 echo "----------------------------------------"
 cd frameworks/base
 echo "----------------------------------------"
-echo "nothing for now..."
+git fetch https://gerrit.omnirom.org/android_frameworks_base refs/changes/31/16931/1 && git cherry-pick FETCH_HEAD
 echo "----------------------------------------"
 cd ../..
 echo "----------------------------------------"
 echo "----------------------------------------"
 cd frameworks/native
 echo "----------------------------------------"
-echo "nothing for now..."
+git fetch https://gerrit.omnirom.org/android_frameworks_native refs/changes/48/16848/3 && git cherry-pick FETCH_HEAD
 echo "----------------------------------------"
 cd ../..
 echo "----------------------------------------"
