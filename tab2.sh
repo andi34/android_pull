6d111567295ed6b0ad9a609f6c793da34c9d24db
Andreas Blaesius <skate4life@gmx.de>
2016-06-09 22:22:20 +0200

    Update tab2.sh

--- tab2.sh
+++ tab2.sh
@@ -9,8 +9,7 @@ echo "----------------------------------------"
 echo "----------------------------------------"
 cd $SAUCE/device/samsung/espressowifi
 echo "----------------------------------------"
-echo "espresso: set hwrotation on init for espresso and espressowifi variants"
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_samsung_espressowifi refs/changes/01/138501/1 && git cherry-pick FETCH_HEAD
+echo "nothing for now..."
 echo "----------------------------------------"
 echo "----------------------------------------"
 cd $SAUCE/device/samsung/espresso3g
@@ -19,18 +18,18 @@ echo "nothing for now..."
 echo "----------------------------------------"
 echo "----------------------------------------"
 cd $SAUCE/hardware/samsung
+echo "----------------------------------------"
 echo "nothing for now..."
 echo "----------------------------------------"
 echo "----------------------------------------"
 cd $SAUCE/hardware/ti/omap4
 echo "----------------------------------------"
-git fetch https://gerrit.omnirom.org/android_hardware_ti_omap4 refs/changes/83/17483/2 && git cherry-pick FETCH_HEAD
-git fetch https://gerrit.omnirom.org/android_hardware_ti_omap4 refs/changes/84/17484/2 && git cherry-pick FETCH_HEAD
+echo "nothing for now..."
 echo "----------------------------------------"
 echo "----------------------------------------"
 cd $SAUCE/kernel/samsung/espresso10
 echo "----------------------------------------"
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_kernel_samsung_espresso10 refs/changes/01/135101/1 && git cherry-pick FETCH_HEAD
+echo "nothing for now..."
 echo "----------------------------------------"
 echo "----------------------------------------"
 cd $SAUCE/frameworks/av
@@ -71,13 +70,10 @@ echo "nothing for now..."
 echo "----------------------------------------"
 echo "----------------------------------------"
 cd $SAUCE/vendor/samsung
-git fetch https://github.com/andi34/proprietary_vendor_samsung.git android-6.0
 echo "----------------------------------------"
-echo "espresso-common: remove the unused libQmageDecoder library"
-git cherry-pick 4e04ab19a3b7888b3a00842868e6c4931b4301bd
+echo "nothing for now..."
 echo "----------------------------------------"
-echo "espresso-common: update widevine from sprout"
-git cherry-pick 012233034bc3c0f265dca0e6d2d250d9570180b8
+echo "----------------------------------------"
 echo ""
 cd $SAUCE
 echo "Done!"
