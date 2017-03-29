44047de8ec503c050da7aff16609b883a582c360
Andreas Blaesius <skate4life@gmx.de>
2017-03-29 23:18:43 +0200

    Add a note about additions

--- patch.sh
+++ patch.sh
@@ -8,8 +8,25 @@ PATCHREPOS=(
     'packages/apps/Settings'
 )
 
+echo "----------------------------------------"
+echo "----------------------------------------"
+echo "applying AOSP additions:"
+echo "Fixes for devices with abnormal hw rotation"
+echo "BootMessage: Show each app name being optimized during boot"
+echo "Add alternate WebView provider package"
+echo "screenshot: quick delete action"
+echo "advanced reboot menu"
+echo "Show application's package name in the 'App info' screen."
+echo "Open app when clicking on icon in App Info screen"
+echo "----------------------------------------"
+echo "----------------------------------------"
+
 for patches in "${PATCHREPOS[@]}"; do
     cd "${ROOT}/${patches}"
     git am --whitespace=nowarn "${PATCHPATH}/${patches}"/*
     cd "${ROOT}"
 done
+
+echo "----------------------------------------"
+echo "Done!"
+echo "----------------------------------------"
