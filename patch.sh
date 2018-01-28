828172a341a99b4238596b6e07280ca701ca654c
Andreas Blaesius <skate4life@gmx.de>
2018-01-28 01:28:48 +0100

    Use the standard ION driver

---
+++ patch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ROOT="$SAUCE"
+PATCHPATH="$SAUCE/pull"
+PATCHREPOS=(
+    'hardware/ti/omap4'
+    'kernel/ti/omap4'
+)
+
+for patches in "${PATCHREPOS[@]}"; do
+    cd "${ROOT}/${patches}"
+    git am --whitespace=nowarn "${PATCHPATH}/${patches}"/*
+    cd "${ROOT}"
+done
