d78637d96dddbe365a49dfcf417ac4c43c84e635
Andreas Blaesius <andi@unlegacy-android.org>
2018-05-20 12:19:41 +0200

    Run git am --abort if patching fails
    Change-Id: Ibb3de72c5de710dfc55235eb2ec493f6888021b7

--- patch.sh
+++ patch.sh
@@ -8,6 +8,6 @@ PATCHREPOS=(
 
 for patches in "${PATCHREPOS[@]}"; do
     cd "${ROOT}/${patches}"
-    git am --whitespace=nowarn "${PATCHPATH}/${patches}"/*
+    git am --whitespace=nowarn "${PATCHPATH}/${patches}"/* || git am --abort
     cd "${ROOT}"
 done
