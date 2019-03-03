1fd7dab9ce8c32185fc53c07a9f59ced6e0468fd
Andreas Blaesius <andi@unlegacy-android.org>
2019-03-03 22:45:18 +0100

    Remove merged parts, tuna is not a thing atm
    Change-Id: I4206eee4df6eb68c662d14e4eba4bb160a32d000

--- patch.sh
+++ patch.sh
@@ -5,8 +5,6 @@ PATCHPATH="$SAUCE/pull"
 PATCHREPOS=(
     'build'
     'device/samsung/espresso3g'
-    'device/samsung/espressowifi'
-    'device/samsung/tuna'
     'frameworks/av'
     'hardware/ti/omap4'
 )
