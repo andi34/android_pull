01199497353c240b70ad715ff98c4bf52d340b27
Andreas Blaesius <andi@unlegacy-android.org>
2020-04-21 17:40:24 +0200

    Fix flex on Ubuntu 18
    Change-Id: I6085184c2eb4bd7eb7e0c93ef98369b6a4763b5f

--- patch.sh
+++ patch.sh
@@ -6,11 +6,12 @@ PATCHREPOS=(
     'build'
     'frameworks/av'
     'frameworks/native'
+    'prebuilts/misc'
     'vendor/cm'
 )
 
 for patches in "${PATCHREPOS[@]}"; do
     cd "${ROOT}/${patches}"
-    git am --whitespace=nowarn "${PATCHPATH}/${patches}"/*
+    git am --whitespace=nowarn "${PATCHPATH}/${patches}"/* || git am --abort
     cd "${ROOT}"
 done
